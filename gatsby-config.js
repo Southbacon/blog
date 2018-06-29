04baa01cb5973616ae1cca9768f7aacb8bfbd883
Ellie Strejlau <estrejlau@phase2technology.com>
2018-06-29 13:20:01 -0400

    Change Gatsby config to Southbacon stuff.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'The Southbacon Blog',
+    author: 'Southbacon',
+    description: 'The Southbacon blog',
+    siteUrl: 'https://southbacon.github.io/blog/',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/blog',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
